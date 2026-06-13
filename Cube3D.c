8a71197324c394ff863c3c4dde6c57e40c53814f
MountainEnjoyer <chauve.zakary@gmail.com>
2026-06-13 11:29:26 +0200

    Cube with wires

--- Cube3D.c
+++ Cube3D.c
@@ -9,20 +9,25 @@ int main(int argc, char *argv[]) {
 
   SetTargetFPS(60);
   
-  Vector3 pos = {10,0,0};
+  Vector3 pos = {5,0,0};
   Vector3 target = {0,0,0};
   Vector3 up = {0,0,1};
-  float fovy = 90; 
+  float fovy = 90;
+
   Camera3D cam = {pos, target, up, fovy, CAMERA_PERSPECTIVE};
-  BeginMode3D(cam);
+  DisableCursor();
+
   while (!WindowShouldClose()) {
-    Camera3D cam = {pos, target, up, fovy, CAMERA_PERSPECTIVE};
-  
+ 
+    UpdateCamera(&cam, CAMERA_FREE);
+ 
     BeginDrawing();
+    ClearBackground(BLACK);
     BeginMode3D(cam);
 
-    Vector3 origin = {0,0,0};
+    Vector3 origin = {0, 0,0};
     DrawCube(origin, 1,1,1, RED);
+    DrawCubeWires(origin, 1,1,1, WHITE);
     
     EndMode3D();
     EndDrawing();
