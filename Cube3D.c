6a36c12d34320a5995b96ae9bc5c12dd26431371
MountainEnjoyer <chauve.zakary@gmail.com>
2026-06-13 10:59:37 +0200

    Added Camera and Cube

--- Cube3D.c
+++ Cube3D.c
@@ -8,10 +8,25 @@ int main(int argc, char *argv[]) {
   InitWindow(WIDTH, HEIGHT, "3D Cube");
 
   SetTargetFPS(60);
+  
+  Vector3 pos = {10,0,0};
+  Vector3 target = {0,0,0};
+  Vector3 up = {0,0,1};
+  float fovy = 90; 
+  Camera3D cam = {pos, target, up, fovy, CAMERA_PERSPECTIVE};
+  BeginMode3D(cam);
   while (!WindowShouldClose()) {
+    Camera3D cam = {pos, target, up, fovy, CAMERA_PERSPECTIVE};
+  
     BeginDrawing();
+    BeginMode3D(cam);
 
+    Vector3 origin = {0,0,0};
+    DrawCube(origin, 1,1,1, RED);
+    
+    EndMode3D();
     EndDrawing();
   }
+
   return 0;
 }
